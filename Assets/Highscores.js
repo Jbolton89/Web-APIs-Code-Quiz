a0b4b10a009ec4b250402b2d68b027b16ccea0e0
Jbolts <77908382+Jbolton89@users.noreply.github.com>
2021-03-30 18:02:30 +1100

    Final commit for now

--- Assets/Highscores.js
+++ Assets/Highscores.js
@@ -1,7 +1,19 @@
 var win = document.querySelector(".win")
 var lose = document.querySelector(".lose")
 
+var winCounter = 0; 
+var loseCounter = 0; 
+
 function init() {
     getWins();
     getLosses();
 }
+
+function winGame () {
+    if (countDown >= 1){ 
+        winCounter++ 
+    }
+}
+localStorage.setItem('winCounter'),JSON.stringify(winCounter);
+localStorage.setItem('loseCounter'),JSON.stringify(loseCounter);
+// I ran out of time to learn this again 
--- Assets/script.js
+++ Assets/script.js
@@ -4,14 +4,16 @@ var nextButton = document.querySelector(".next-btn")
 var questionContainerEl = document.getElementById("question-container")
 var questionEl = document.getElementById('question')
 var answerButtonsEl = document.getElementById('answer-buttons')
+var timeLeft = 60;
+
 
 var randomQuestion, currentQuestionIndex
 
-nextButton.addEventListener('click', ()=> {
-        currentQuestionIndex++
-        nextQuestion()
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    nextQuestion()
 })
-startButton.addEventListener("click", function(){
+startButton.addEventListener("click", function () {
     countDown();
     console.log('started');
     startButton.classList.add('hide');
@@ -29,47 +31,48 @@ function nextQuestion() {
 function showQuestion(question) {
     questionEl.innerText = question.question;
     question.answers.forEach(function (answer) {
-            var button = document.createElement('button')
-            button.textContent = answer.text
-            button.classList.add('btn')
-            if (answer.correct) {
-                button.dataset.correct = answer.correct
-            }
-            button.addEventListener('click', selectAnswer)
-            answerButtonsEl.appendChild(button)
-        })
+        var button = document.createElement('button')
+        button.textContent = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsEl.appendChild(button)
+    })
 }
-function resetFunction() { 
+function resetFunction() {
     nextButton.classList.add('hide')
     while (answerButtonsEl.firstChild) {
         answerButtonsEl.removeChild(answerButtonsEl.firstChild)
-        }
     }
+}
 function selectAnswer(e) {
     var selectedButton = e.target
     var correct = selectedButton.dataset.correct
     // setStatusClass(document.getElementsByClassName('btn'))
     // .classList.add(correct)
     Array.from(answerButtonsEl.children).forEach(function (button) {
-            setStatusClass(button, button.dataset.correct)
-        })
-        if (randomQuestion.length > currentQuestionIndex + 1) {
-            nextButton.classList.remove('hide')
-        } else {
-            startButton.innerText = 'Restart'
-            startButton.classList.remove('hide')
-            startButton.disabled = false;
-        }
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (randomQuestion.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+        startButton.disabled = false;
     }
-    
+}
+
 // I am quite sure it is to do with the above
 
-function setStatusClass(element, correct) { 
+function setStatusClass(element, correct) {
     clearStatusClass(element)
-    if (correct) { 
-        element.classList.add('correct') 
-    } else { 
-        element.classList.add('wrong')
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong');
+        timeLeft-=5; 
     }
 }
 
@@ -83,34 +86,33 @@ var questions = [
         question: 'Which of these is NOT a data type?',
         answers: [
             { text: 'Alerts', correct: true },
-            { text: 'Booleans', correct: false }, 
-            { text: 'Strings', correct: false }, 
-            { text: 'Numbers', correct: false}
-        ], 
+            { text: 'Booleans', correct: false },
+            { text: 'Strings', correct: false },
+            { text: 'Numbers', correct: false }
+        ],
     },
     {
         question: 'What is the largest part of the brain?',
         answers: [
             { text: 'Amygdala', correct: false },
-            { text: 'Cerebellum', correct: false }, 
-            { text: 'Cerebrum', correct: true }, 
-            { text: 'Front part', correct: false}
-        ], 
+            { text: 'Cerebellum', correct: false },
+            { text: 'Cerebrum', correct: true },
+            { text: 'Front part', correct: false }
+        ],
     },
     {
         question: 'Which of these is a programming language?',
         answers: [
             { text: 'Bite', correct: false },
-            { text: 'Gnaw', correct: false }, 
-            { text: 'Itch', correct: false }, 
-            { text: 'Scratch', correct: true}
-        ], 
+            { text: 'Gnaw', correct: false },
+            { text: 'Itch', correct: false },
+            { text: 'Scratch', correct: true }
+        ],
     }
 
 ]
 // For countdown timer - Now fully functional except the penalties which I am waiting to fix the above
 function countDown() {
-    var timeLeft = 60;
     var timeInterval = setInterval(function () {
         if (timeLeft > 1) {
             timerEl.textContent = timeLeft + ' seconds remaining';
@@ -123,7 +125,6 @@ function countDown() {
             clearInterval(timeInterval);
         }
     }, 1000)
-    startButton.disabled = true;
+    startButton.disabled = true; 
 }
-// startButton.disabled = true; 
-// Need to figure out how to disable this button once clicked
+
